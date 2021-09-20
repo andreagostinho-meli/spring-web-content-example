d8538dbad889555100af4b83666a1df65ef4777f
Andre Carlos Agostinho <andre.agostinho@mercadolivre.com>
2021-09-20 13:50:24 -0300

    initial commit

---
+++ src/main/java/br/com/meli/springwebcontentexample/SpringWebContentExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.springwebcontentexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebContentExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebContentExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/meli/springwebcontentexample/SpringWebContentExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.springwebcontentexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebContentExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
